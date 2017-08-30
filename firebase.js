ee08334ffd9fd8d579e9c199c213cb5feb357630
DD-EDH-N6 <michaelcurtis96@gmail.com>
2017-08-30 10:10:01 -0700

    auth 3

--- firebase.js
+++ firebase.js
@@ -12,13 +12,14 @@ console.log("firebase loaded.");
 
 var provider = new firebase.auth.GoogleAuthProvider();
 
+console.log("signing in");
 firebase.auth().signInWithPopup(provider).then(function(result) {
   // This gives you a Google Access Token. You can use it to access the Google API.
   var token = result.credential.accessToken;
   // The signed-in user info.
   var user = result.user;
   // ...
-  alert("signing in");
+  console.log("signing in success");
 }).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
@@ -30,3 +31,5 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
   // ...
   console.log("failed to sign in: " + errorCode + ": " + errorMessage);
 });
+
+console.log("done signing in");
