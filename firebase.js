1deff0b06d68fc5220cd8e2a35cd52c4f62f4cb0
DD-EDH-N6 <michaelcurtis96@gmail.com>
2017-08-30 09:50:42 -0700

    test server

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBEFgCJYx1yHWZiqFVLyydoV_Ixd8HTAIg",
+  authDomain: "picsandcr.firebaseapp.com",
+  databaseURL: "https://picsandcr.firebaseio.com",
+  projectId: "picsandcr",
+  storageBucket: "picsandcr.appspot.com",
+  messagingSenderId: "425685988468"
+};
+firebase.initializeApp(config);
+console.log("firebase loaded.");
