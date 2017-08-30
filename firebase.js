4fe24de5f71443f1d4e57a100bc3ed4adb66e5cb
DD-EDH-N6 <michaelcurtis96@gmail.com>
2017-08-30 10:56:42 -0700

    v10

--- firebase.js
+++ firebase.js
@@ -17,28 +17,30 @@ var provider = new firebase.auth.GoogleAuthProvider();
 console.log("signing in");
 if (!signedIn) {
   firebase.auth().signInWithRedirect(provider);
+
+  firebase.auth().getRedirectResult().then(function(result) {
+    if (result.credential) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // ...
+      console.log("signed in: " + token);
+      signedIn = 1;
+    }
+    // The signed-in user info.
+    var user = result.user;
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
 }
 
 
-firebase.auth().getRedirectResult().then(function(result) {
-  if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // ...
-    console.log("signed in: " + token);
-    signedIn = 1;
-  }
-  // The signed-in user info.
-  var user = result.user;
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
+
 
 console.log("done signing in");
